997de3b1190686bffd65b86a817e68ff592e62bc
Suhaibusman <suhaibusman54@gmail.com>
2024-01-28 00:30:01 +0500

    netflix clone

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:netflix_clone/screen/splash/splash_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
